2236e2d8dae7c8882fc6bcebb41ac66c96292dea
jrdutra <jrdutra.com.br@gmail.com>
2020-09-11 16:55:35 -0300

    Studing-Angular

--- MaximilianSchwarzmuller/basics-assignment-3-start/src/app/app.component.ts
+++ MaximilianSchwarzmuller/basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  arrLog = [
+    {
+      'number':'0',
+      'time': new Date().toString()
+    }, 
+    {
+      'number':'1',
+      'time': new Date().toString()
+    }
+  ];
+
+  myStyle = {'color': 'red'};
+
+  getStyle(){
+    return this.myStyle;
+  }
+
+  creatNewArrLogObj(){
+    return {
+      'number': this.arrLog.length.toString(),
+      'time': new Date().toString()
+    }
+  }
+
+  getColor(number){
+    if(number>4){
+      return 'blue';
+    }
+    return 'white';
+  }
+
+  onBtnClick(){
+    if(this.myStyle.color == 'red'){
+      this.myStyle.color = 'black'
+    }else{
+      this.myStyle.color = 'red'
+    }
+    this.arrLog.push(this.creatNewArrLogObj());
+  }
+
 }
