d32fced6e9a47302002ee5efd97de52b121cbad3
jrdutra <jrdutra.com.br@gmail.com>
2020-09-11 15:47:03 -0300

    Studing-Angular

--- MaximilianSchwarzmuller/basics-assignment-2-start/src/app/app.component.ts
+++ MaximilianSchwarzmuller/basics-assignment-2-start/src/app/app.component.ts
@@ -6,4 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  varName : string = "";
+  varEmpty : boolean = true;
+
+  onKey(event: any) { // without type info
+    if(this.varName.length == 0){
+      this.varEmpty = true;
+    }else{
+      this.varEmpty = false;
+    }
+  }
+
+  clearName(){
+    this.varName = "";
+    this.varEmpty = true;
+  }
+
 }
