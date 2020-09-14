8bb72279a936bac66e2abb3722230f55bd4ad996
jrdutra <jrdutra.com.br@gmail.com>
2020-09-14 11:17:42 -0300

    Studing-Angular

---
+++ MaximilianSchwarzmuller/angular-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simple a test', 'https://www.telegraph.co.uk/content/dam/food-and-drink/2019/01/11/TELEMMGLPICT000185036503_trans_NvBQzQNjv4BqodXSHHE-j78vyZ0iwRUmY_nuzprQ_mxVCWqrJBTJk3A.jpeg')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
