7edc8e9d6860ea61cfa74bec6a2f75e780271cf1
jrdutra <jrdutra.com.br@gmail.com>
2020-09-14 11:55:06 -0300

    Studing-Angular

--- MaximilianSchwarzmuller/angular-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ MaximilianSchwarzmuller/angular-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from '../recipe.model'
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,7 +9,8 @@ import { Recipe } from '../recipe.model'
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a simple a test', 'https://www.telegraph.co.uk/content/dam/food-and-drink/2019/01/11/TELEMMGLPICT000185036503_trans_NvBQzQNjv4BqodXSHHE-j78vyZ0iwRUmY_nuzprQ_mxVCWqrJBTJk3A.jpeg')
+    new Recipe('A Test Recipe', 'This is a simple a test', 'https://www.telegraph.co.uk/content/dam/food-and-drink/2019/01/11/TELEMMGLPICT000185036503_trans_NvBQzQNjv4BqodXSHHE-j78vyZ0iwRUmY_nuzprQ_mxVCWqrJBTJk3A.jpeg'),
+    new Recipe('A Tasty HotDog', 'This is a simples test of a recipe', 'https://image.shutterstock.com/image-photo/hot-dog-mustard-ketchup-side-260nw-683413162.jpg')
   ];
 
   constructor() { }
--- MaximilianSchwarzmuller/angular-project/src/app/recipes/recipe.model.ts
+++ MaximilianSchwarzmuller/angular-project/src/app/recipes/recipe.model.ts
@@ -6,11 +6,8 @@ export class Recipe {
     public imagePath: string;
 
     constructor(name: string, desc: string, imagePath: string){
-
         this.name = name;
         this.description = desc;
         this.imagePath = imagePath;
-
     }
-
 }
