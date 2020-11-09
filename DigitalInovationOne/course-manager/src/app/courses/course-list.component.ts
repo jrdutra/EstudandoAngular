22131ac169494790b8370a60720f61da72323f32
jrdutra <jrdutra.com.br@gmail.com>
2020-11-09 08:27:06 -0300

    Studing-Angular

--- DigitalInovationOne/course-manager/src/app/courses/course-list.component.ts
+++ DigitalInovationOne/course-manager/src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component({
   selector: 'app-course-list',
@@ -9,29 +10,10 @@ export class CourseListComponent implements OnInit {
 
   courses: Course[] = [];
 
+  constructor(private courseService: CourseService){ }
+
   ngOnInit(): void {
-    this.courses = [
-      {
-        id: 1,
-        name: 'Angular Forms',
-        imageUrl: '',
-        price: 99.99,
-        code: 'XPS-8796',
-        duration: 120,
-        rating: 5.4,
-        releaseDate: 'December, 2, 2019'
-      },
-      {
-        id: 2,
-        name: 'Angular HTTP',
-        imageUrl: '',
-        price: 45.99,
-        code: 'LKL-8796',
-        duration: 80,
-        rating: 4.0,
-        releaseDate: 'December, 3, 2019'
-      },
-    ]
+    this.courses = this.courseService.retrieveAll();
   }
 
 
